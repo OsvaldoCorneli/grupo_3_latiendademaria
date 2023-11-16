d92aa6cf8c5da2f4b76ba3bcb690323f54600b27
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-11-16 18:02:06 -0300

    agregando registro . modificando algunos html

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.route("/profile")
     })
 ;
 
+app.route("/register")
+    .get((req,res) => {
+        res.sendFile(path.join(__dirname,'/views/registro.html'))
+    })
+;
+
 app.listen(3001,(req,res) => {
     console.log(`Server corriendo en puerto http://localhost:3001`)
 });
