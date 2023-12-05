f14a68721cebc4bff788540117e7c8cd3aa20167
Gustavo Rodolfo Paz <gusti.paz@gmail.com>
2023-12-04 22:26:14 -0300

    ok

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const morgan = require('morgan')
 const path = require('path');
 const app = express();
 const routes = require('./routes')
+const methodOverride = require('method-override');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}))
-app.use(morgan('dev'))
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 //app.set('views', './carpeta-de-vistas')   <<--- ejemplo de codigo a usar si se quiere cambiar la ruta views por defecto(./views).
 
-
-app.use('/', routes)
-
+app.use('/', routes);
 
 app.listen(3001,(req,res) => {
     console.log(`Server corriendo en puerto http://localhost:3001`)
