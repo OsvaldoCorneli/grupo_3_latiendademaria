54ce1f696a956d6cd8110e16561976156ece24be
Gustavo Rodolfo Paz <gusti.paz@gmail.com>
2023-12-15 21:43:45 -0300

    agrego midlwares y carpeta views de products

--- controllers/product.js
+++ controllers/product.js
@@ -1,28 +1,32 @@
+const path = require('path')
 const { products } = require('../models')
+
+const view = path.join(__dirname,'../views/products/');
 
 const productsController = {
     index: function (req,res) {
         let { id } = req.params
-        if (!id && req.method == 'POST') {
-            res.render('products', { 
+        if (!id && req.method == 'POST') { //este if es para el filtro
+            res.render(view+'products', { 
                 productos: products.filter(req.body),
                 categorias: products.categories(),
                 colors: products.colors()
             })
-        } else if (!id && req.method == 'GET') {
-            res.render('products', { 
+        } else if (!id && req.method == 'GET') { // este if es para primera vista o resetear filtro
+            res.render(view+'products', { 
                 productos: products.all(),
                 categorias: products.categories(),
                 colors: products.colors()
             })
         } else {
-            res.render('detail',{
-                detalle: products.detail(id)
-            })
+            const detalle = products.detail(id)
+            if (detalle) res.render(view+'detail',{ detalle }) // si no, renderiza el detalle de producto que recibo por params
+            else res.render('404notFound', {url: req.url}) // si no encuentra el producto, devuelve 404
+            
         }
     },
     create: function (req,res) {
-        res.render('createForm', {
+        res.render(view+'createForm', {
             categorias: products.categories(),
             colors: products.colors()
         })
