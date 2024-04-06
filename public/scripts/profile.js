1a94239bde4294d96888147c8fe403eb045ec001
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2024-04-06 18:34:29 -0300

    agregue popUp para eliminacion del usuario

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,3 +1,32 @@
+
+function preEliminacion(id){
+    const body = document.querySelector("body")
+    const popup = document.createElement('span');
+    popup.classList.add('popupscreen');
+    popup.innerHTML = `
+        <div class="popUps" id="popUpDetailLogin">
+            <h3>¿Está seguro de que desea proceder con la eliminación de su cuenta?</h3>
+            <h4>Esta acción resultará en la eliminación permanente de todos los datos asociados con su cuenta.</h4>
+            <div class="botonPopup">
+                <a onclick="eliminarUsuario('${id}')">Aceptar</a>
+                <a onclick="popUpoOff()">Cancelar</a>
+            </div>  
+        </div>
+    `;
+    body.appendChild(popup);
+
+}
+
+
+function popUpoOff(){
+    const body = document.querySelector("body")
+    const elementosPopup = document.querySelector('.popupscreen');
+    
+    body.removeChild(elementosPopup);
+    
+}
+
+
 function eliminarUsuario(id) {
     
     if (confirm("¿Está seguro/a de que desea eliminar su usuario?")) {
