3127e03210450430d86e6737dbcd6fd3bc8a20a6
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2024-04-06 18:22:22 -0300

    hice pull origin develop, modifique profile usuario administrador

--- public/scripts/subMenu.js
+++ public/scripts/subMenu.js
@@ -10,26 +10,42 @@ botones.forEach(element => {
             case "Mis compras":
                 payment.style.display = "flex"
                 favorite.style.display = "none"
-                deletedCount.style.display = "none"
                 changePass.style.display = "none"
+                if(deletedCount){
+                deletedCount.style.display = "none"
+                }
+                console.log("ingresa mis compras")
             break
             case "Mis favoritos":
                 favorite.style.display = "flex"
                 payment.style.display = "none"
-                deletedCount.style.display = "none"
                 changePass.style.display = "none"
+                if(deletedCount){
+                    deletedCount.style.display = "none"
+                    }
+                console.log("ingresa favoritos")
             break   
             case "Eliminar Cuenta":
                 deletedCount.style.display = "flex"
                 payment.style.display = "none"
                 favorite.style.display = "none"
                 changePass.style.display = "none"
+                console.log("eliminar cuenta")
             break
             case "Cambiar contraseña":
                 changePass.style.display = "flex"
-                deletedCount.style.display = "none"
                 payment.style.display = "none"
                 favorite.style.display = "none"
+                if(deletedCount){
+                    deletedCount.style.display = "none"
+                    }
+                console.log("cambiar contraseña")
+            break
+            case "Mis productos":
+                payment.style.display = "flex"
+                favorite.style.display = "none"
+                changePass.style.display = "none"
+                console.log("mis productos")
             break
             default:
             break
